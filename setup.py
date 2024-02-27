05e4cc3d29bd1b67686ef85405c20755279a1e96
Minh-Tri <minhtri.pham@winnowsolutions.com>
2024-02-27 15:20:21 +0000

    updated version.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup, find_namespace_packages
 
-from mt.net.version import version
+VERSION_FILE = os.path.join(os.path.dirname(__file__), "VERSION.txt")
 
 install_requires = [
-    "mtbase>=4.16",  # mtbase without mt.net
+    "mtbase>=4.26",  # just updating
     "getmac<0.9",  # a bug at getmac>=0.9 is stopping us from using get_mac_address properly
     "netifaces",
     "sshtunnel",  # for ssh tunnelling
@@ -13,7 +14,6 @@ install_requires = [
 
 setup(
     name="mtnet",
-    version=version,
     description="The most fundamental Python modules for Minh-Tri Pham",
     author=["Minh-Tri Pham"],
     packages=find_namespace_packages(include=["mt.*"]),
@@ -22,5 +22,14 @@ setup(
     project_urls={
         "Documentation": "https://mtdoc.readthedocs.io/en/latest/mt.net/mt.net.html",
         "Source Code": "https://github.com/inteplus/mtnet",
+    },
+    setup_requires=["setuptools-git-versioning<2"],
+    setuptools_git_versioning={
+        "enabled": True,
+        "version_file": VERSION_FILE,
+        "count_commits_from_version_file": True,
+        "template": "{tag}",
+        "dev_template": "{tag}.dev{ccount}+{branch}",
+        "dirty_template": "{tag}.post{ccount}",
     },
 )
